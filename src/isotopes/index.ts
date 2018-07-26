2075c8770852f2d9c14fcc50ce3d7f5957517d87
squidfunk <martin.donath@squidfunk.com>
2018-07-26 12:20:12 +0200

    Added support for configuration of parameter and return types

--- src/isotopes/index.ts
+++ src/isotopes/index.ts
@@ -42,10 +42,10 @@ import {
  * @template T - Data type
  */
 export interface IsotopeOptions<T extends {}> {
-  domain: string                       /* SimpleDB domain name */
-  key: keyof T                         /* SimpleDB item name (primary key) */
   format?: IsotopeFormatOptions        /* Format options */
   client?: IsotopeClientOptions        /* Client options */
+  domain: string                       /* SimpleDB domain name */
+  key: keyof T                         /* SimpleDB item name (primary key) */
 }
 
 /* ----------------------------------------------------------------------------
@@ -55,9 +55,38 @@ export interface IsotopeOptions<T extends {}> {
 /**
  * Isotope
  *
+ * By default this library forces only valid entries to be written to SimpleDB
+ * which means that all non-optional fields need to be defined in the payload.
+ * However, SimpleDB allows reading and writing of partial attribute values,
+ * so it might be desirable in some cases to loosen that restriction and allow
+ * partial reads and writes. Isotope allows both configurations through simple
+ * generic typing.
+ *
+ * The first type argument is mandatory and defines the base type. The second
+ * and third type arguments can be used to specify what exact types put and get
+ * operations return but normally they are equal to the base type.
+ *
+ * @example <caption>Allow complete values only</caption>
+ *
+ *   new Isotope<Type>
+ *
+ * @example <caption>Allow partial values in put and get operations</caption>
+ *
+ *   new Isotope<Type, Partial<Type>>
+ *
+ * @example <caption>Allow partial values in get operations only</caption>
+ *
+ *   new Isotope<Type, Type, Partial<Type>>
+ *
  * @template T - Data type
+ * @template TGet - Data type returned by put operation
+ * @template TPut - Data type returned by get operation
  */
-export class Isotope<T extends {}> {
+export class Isotope<
+  T    extends {},
+  TPut extends Partial<T> = T,
+  TGet extends Partial<T> = T
+> {
 
   /**
    * SimpleDB client
@@ -81,14 +110,12 @@ export class Isotope<T extends {}> {
    *
    * @return Promise resolving with (partial) item data
    */
-  public async get(id: string): Promise<T | undefined>
-  public async get(id: string, names: string[]): Promise<Partial<T> | undefined>
   public async get(
     id: string, names?: string[]
-  ): Promise<T | Partial<T> | undefined> {
+  ): Promise<TGet | undefined> {
     const item = await this.client.get(id, names)
     if (item) {
-      const data: T = decode(item.attrs, this.options.format)
+      const data: TGet = decode(item.attrs, this.options.format)
       data[this.options.key] = item.id as any // TODO: Fix types
       return data
     }
@@ -102,7 +129,7 @@ export class Isotope<T extends {}> {
    *
    * @return Promise resolving with no result
    */
-  public async put(data: Partial<T>): Promise<void> {
+  public async put(data: TPut): Promise<void> {
     await this.client.put(
       data[this.options.key].toString(),
       encode(
